d2fa106dc2e08d91b3b7cd7f93f0f746b61f1a46
Mafyoc <mattpanting123@hotmail.co.uk>
2023-03-02 15:38:58 +0000

    Refactored index.php, StyleSheet.css, submit-form.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype HTML>
 <html>
 <head>
-    <title>Matts Activstep challenge</title>
+    <title>Matts Customer Details</title>
     <link rel="stylesheet" href="StyleSheet.css" />
 </head>
 <body>
@@ -12,45 +12,47 @@
         include('connect-mysql.php');
         $sql = "SELECT * FROM customers";
         $result = $conn->query($sql);
-        //this function dynamically outputs a basic HTML table from the mysql result
         function createTable($result) {
-            echo "<table border='1'>
+            if($result->num_rows > 0){
+                echo "<table border='1'>
             <tr>
-                <th>id</th>
+                <th>ID</th>
                 <th>Name</th>
                 <th>Phone</th>
                 <th>Email</th>
                 <th>Accref</th>
             </tr>";
 
-            while($row = mysqli_fetch_array($result))
-            {
-                echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['name'] . "</td>";
-                echo "<td>" . $row['phone'] . "</td>";
-                echo "<td>" . $row['email'] . "</td>";
-                echo "<td>" . $row['accref'] . "</td>";
-                echo "</tr>";
+                while($row = mysqli_fetch_array($result))
+                {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['name'] . "</td>";
+                    echo "<td>" . $row['phone'] . "</td>";
+                    echo "<td>" . $row['email'] . "</td>";
+                    echo "<td>" . $row['accref'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }   //this function dynamically outputs a basic HTML table from the mysql result
+            else{
+                echo "0 entries in database";
             }
-            echo "</table>";
-        }
+        }   //creates a table or outputs that there are no entries
 
-        if ($result->num_rows > 0) {
-            // output data of each row
-            echo createTable($result);
-        } else {
-            echo "0 entries in database";
+        if(isset($_POST['return'])) {
+            createTable($result);
+        } else{
+            createTable($result);
         }
 
         $conn->close();
         ?>
     </div>
-
     <form action="submit-form.php" method="post">
-        <label>ID: </label> <input type="number" name="id" placeholder="ID" /><br />
+        <label>ID (enter an existing ID to update or leave blank for new entry): </label> <input type="number" name="id" placeholder="ID" /><br />
         <label>Name: </label><input type="text" name="name" placeholder="Name" /><br />
-        <label>Phone: </label><input type="text" name="phone" placeholder="Phone" /><br />
+        <label>Phone: </label><input type="tel" name="phone" placeholder="Phone" /><br />
         <label>Email: </label><input type="email" name="email" placeholder="Email" /><br />
         <label>Accref: </label><input type="text" name="accref" placeholder="Accref" /><br />
         <input type="submit" value="submit" name="submit" id="submit" />
--- submit-form.php
+++ submit-form.php
@@ -7,45 +7,49 @@
 <body>
     <?php //submit form details to mysql
     include('connect-mysql.php');
-
     $id = $_POST['id'];
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
     $accref = $_POST['accref'];
 
-    $result = $conn->query("SELECT * FROM customers WHERE id = '$id'");
+    $result = $conn->query("SELECT * FROM customers WHERE id");
+    function newEntry($conn, $id, $name, $phone, $email, $accref){
+        $sql = "INSERT INTO customers (id, name, phone, email, accref) VALUES ('$id', '$name', '$phone', '$email', '$accref')";
+        $rs = $conn->query($sql);
 
-    if($result->num_rows == 0){     //checks if the submitted id already exists
-        if(isset($_POST['submit']) && $_POST['id']!=NULL){  
-
-            $sql = "INSERT INTO customers (id, name, phone, email, accref) VALUES ('$id', '$name', '$phone', '$email', '$accref')";
-            $rs = $conn->query($sql);
-
-            if($rs){
-                echo 'Your entry has been added successfully';
-            } 
-            else{
-                echo 'ERROR entry not added';
-            }
+        if($rs){
+            echo 'New entry successfully added';
         }
-    }           //inserts new entry to database if number of rows with submitted id is 0
-    else{
-        if(isset($_POST['submit']) && $_POST['id']!=NULL)
-
+        else{
+            echo 'ERROR entry not added';
+        }
+    }
+    function updateEntry($conn, $id, $name, $phone, $email, $accref){
         $sql = "UPDATE customers SET id = '$id', name = '$name', phone = '$phone', email = '$email', accref = '$accref' WHERE id = '$id'";
         $rs = $conn->query($sql);
 
         if($rs){
-            echo 'Your entry at has been updated successfully';
+            echo 'Entry has been successfully updated';
         } else{
             echo 'ERROR entry not updated';
         }
-    }           //update entry in database if submitted id already exists
+    }
+
+
+    if(($id >= "1") && ($result->num_rows >= $id)){     //checks if the submitted id already exists
+        echo updateEntry($conn, $id, $name, $phone, $email, $accref);
+    }           //updates existing entry when ID is in range
+    else if ($id == NULL){
+        echo newEntry($conn, $id, $name, $phone, $email, $accref);
+    }           //makes a new entry in database if ID is not in range
+    else{
+        echo "INVALID ID";
+    }
     ?>
     <br />
-    <form>
-        <input type="BUTTON" value="Homepage" onclick="window.location.href='https://activtest.uk/matt/'" />
+    <form action="index.php" method="post">
+        <input type="submit" value="Homepage" onclick="window.location.href = 'https://activtest.uk/matt/'" />
     </form>
 
 </body>

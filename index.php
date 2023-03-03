2fc78ba4943140c78ec47e7faffc1f4fd8411759
Mafyoc <mattpanting123@hotmail.co.uk>
2023-03-03 16:23:57 +0000

    refactored index.php, StyleSheet.css submit-form.php

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php //Read data from MySQL
+function createTable(){
+    include('connect-mysql.php');
+    $sql = "SELECT * FROM customers";
+    $result = $conn->query($sql);
+    $newLine="\n";
+
+    if($result->num_rows > 0){
+        //start table
+        $isFirstLine = true;
+        $html .= "<table border='1'>".$newLine;
+
+    while($row = $result->fetch_assoc() )
+    {
+        //Table header
+        if($isFirstLine == true){
+            $html .= "<tr>".$newLine;
+                foreach($row as $key => $value){
+                    $html .= "<th>".$key."</th>".$newLine;
+                }
+            $html .= "<th></th></tr>" . $newLine;
+            $isFirstLine = false;
+            }
+            // table body
+            $html .=   '<tr>'  . $newLine ;
+                foreach ($row as $key => $value ){
+                    $html .=   "<td>" . $value . "</td>"  . $newLine ;
+                }
+                $html .= "<td><input id="."editButton"." "."name="."{$row['id']}"." "."value="."edit"." type="."button"."></td></tr>" . $newLine ;
+        }
+        // closing table
+        $html .=   "</table>"   . $newLine ;
+    }
+
+    return $html;
+}
+?>
+
 <!doctype HTML>
 <html>
 <head>
@@ -6,56 +44,41 @@
 </head>
 <body>
     <h1>Customer Details</h1>
-    <div>
-
-        <?php //Read data from MySQL
-        include('connect-mysql.php');
-        $sql = "SELECT * FROM customers";
-        $result = $conn->query($sql);
-        function createTable($result) {
-            if($result->num_rows > 0){
-                echo "<table border='1'>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>Email</th>
-                <th>Accref</th>
-            </tr>";
-
-                while($row = mysqli_fetch_array($result))
-                {
-                    echo "<tr>";
-                    echo "<td>" . $row['id'] . "</td>";
-                    echo "<td>" . $row['name'] . "</td>";
-                    echo "<td>" . $row['phone'] . "</td>";
-                    echo "<td>" . $row['email'] . "</td>";
-                    echo "<td>" . $row['accref'] . "</td>";
-                    echo "</tr>";
-                }
-                echo "</table>";
-            }   //this function dynamically outputs a basic HTML table from the mysql result
-            else{
-                echo "0 entries in database";
-            }
-        }   //creates a table or outputs that there are no entries
-
-        if(isset($_POST['return'])) {
-            createTable($result);
-        } else{
-            createTable($result);
-        }
-
-        $conn->close();
-        ?>
+    <div style="position:sticky;top:0;">
+        <form action="submit-form.php" method="post" >
+            <div id="formElement">
+                <label>ID: </label>
+                <br />
+                <input type="number" name="id" />
+            </div>
+            <div id="formElement">
+                <label>Name: </label>
+                <br />
+                <input type="text" name="name" />
+            </div>
+            <div id="formElement">
+                <label>Phone: </label>
+                <br />
+                <input type="tel" name="phone" />
+            </div>
+            <div id="formElement">
+                <label>Email: </label>
+                <br />
+                <input type="email" name="email" />
+            </div>
+            <div id="formElement">
+                <label>Accref: </label>
+                <br />
+                <input type="text" name="accref" />
+            </div>
+            <div id="formElement">
+                <input type="submit"  value="submit" name="submit" id="submit"/>
+            </div>
+        </form>
     </div>
-    <form action="submit-form.php" method="post">
-        <label>ID (enter an existing ID to update or leave blank for new entry): </label> <input type="number" name="id" placeholder="ID" /><br />
-        <label>Name: </label><input type="text" name="name" placeholder="Name" /><br />
-        <label>Phone: </label><input type="tel" name="phone" placeholder="Phone" /><br />
-        <label>Email: </label><input type="email" name="email" placeholder="Email" /><br />
-        <label>Accref: </label><input type="text" name="accref" placeholder="Accref" /><br />
-        <input type="submit" value="submit" name="submit" id="submit" />
-    </form>
+    <?php
+        echo createTable();
+    ?>
+
 </body>
 </html>
--- submit-form.php
+++ submit-form.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <link rel="stylesheet" href="StyleSheet.css" />
+    <meta http-equiv="refresh" content="0; url=https://activtest.uk/matt/" />
 </head>
 <body>
     <?php //submit form details to mysql
@@ -18,10 +19,10 @@
         $rs = $conn->query($sql);
 
         if($rs){
-            echo 'New entry successfully added';
+            return 'New entry successfully added';
         }
         else{
-            echo 'ERROR entry not added';
+            return 'ERROR entry not added';
         }
     }
     function updateEntry($conn, $id, $name, $phone, $email, $accref){
@@ -29,27 +30,25 @@
         $rs = $conn->query($sql);
 
         if($rs){
-            echo 'Entry has been successfully updated';
-        } else{
-            echo 'ERROR entry not updated';
+            return 'Entry has been successfully updated';
+        } 
+        else{
+            return 'ERROR entry not updated';
         }
     }
 
 
     if(($id >= "1") && ($result->num_rows >= $id)){     //checks if the submitted id already exists
-        echo updateEntry($conn, $id, $name, $phone, $email, $accref);
+        return updateEntry($conn, $id, $name, $phone, $email, $accref);
     }           //updates existing entry when ID is in range
     else if ($id == NULL){
-        echo newEntry($conn, $id, $name, $phone, $email, $accref);
+        return newEntry($conn, $id, $name, $phone, $email, $accref);
     }           //makes a new entry in database if ID is not in range
     else{
-        echo "INVALID ID";
+        return "INVALID ID";
     }
+    
     ?>
     <br />
-    <form action="index.php" method="post">
-        <input type="submit" value="Homepage" onclick="window.location.href = 'https://activtest.uk/matt/'" />
-    </form>
-
 </body>
 </html>
